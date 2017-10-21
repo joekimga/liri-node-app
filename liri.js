b5738665fa8fedd4adaff61f08cc7d1079c0b802
Joseph <joekimga@gmail.com>
2017-10-21 08:53:41 -0400

    WIP

--- liri.js
+++ liri.js
@@ -1,6 +1,29 @@
- var fs = require("fs");
- var request = require("request");
- var Twitter = require("Twitter");
- var Spotify = require("Spotify");
+var fs = require("fs");
+var request = require("request");
+var Twitter = require("Twitter");
+var Spotify = require("Spotify");
+var keys = require("keys");
 
- 
+var newTwit = new Twitter(keys);
+
+//Twitter 
+
+var parameters = {
+	"screen_name": "gtburneraddy",
+	"count": 20
+}
+
+if (arg === "tweets") {
+	newTwit.get('', parameters,
+	function gotData(error, data, response) {
+		var tweets = data;
+		for(var i =0;i < tweets.lengthl; i++) {
+			console.log(tweets[i].text);
+			console.log(tweets[i].created_at);
+		}
+	});
+	outputText();
+}
+
+//OMDB
+
