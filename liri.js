84a537a7314142851f8af47ec89e8015939c97fc
Joseph <joekimga@gmail.com>
2017-10-22 13:08:57 -0400

    WIP

--- liri.js
+++ liri.js
@@ -25,6 +25,85 @@ if (arg === "tweets") {
 	outputText();
 }
 
-//OMDB
+// Spotify 
+if(argument === "spotify-this-song"){
+    var songTitle = process.argv[3];
+    spotify.search({ type: 'track', query: songTitle }, function(err, data){
+        
+        if(process.argv[3]){
+            var data = data.tracks.items;
+            for(var i =0; i < data.length; i++){
+                
+                console.log(data[i].name);
+                console.log(data[i].album.href);
+                console.log(data[i].album.name);
+                console.log(data[i].preview_url);
+            
+                for(var j =0; j < data[i].artists.length; j++){
+                    console.log(data[i].artists[j].name);
+                }
+            }
+        }else{
+            spotify.search({ type: 'track', query: "The Sign"}, function(err, data){
+                var data = data.tracks.items;
+                console.log(data[0].name); 
+                console.log(data[0].album.href); 
+                console.log(data[0].album.name); 
+                console.log(data[0].preview_url); 
+                console.log(data[0].artists[0].name); 
+            });
+        }
+    });
+    outputText();
+}
 
-if(arg === )
+//OMDB 
+if(argument === "movie-this"){ 
+    console.log(process.argv);
+    var movieTitle = process.argv[3];
+    request("http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&r=json&tomatoes=true",function (error, response, body){
+        
+        if(process.argv[3]){
+        console.log(body);  
+       
+        }else{
+            request("http://www.omdbapi.com/?t=mr+nobody+&y=&plot=short&r=json&tomatoes=true",function(error, response,body){
+                console.log(body);
+            
+            })
+        }
+    })
+    // outputText();
+}
+
+// Spotify Logic
+if(argument === "spotify-this-song"){
+    var songTitle = process.argv[3];
+    spotify.search({ type: 'track', query: songTitle }, function(err, data){
+        
+        if(process.argv[3]){
+            var data = data.tracks.items;
+            for(var i =0; i < data.length; i++){
+                
+                console.log(data[i].name); 
+                console.log(data[i].album.href); 
+                console.log(data[i].album.name); 
+                console.log(data[i].preview_url); 
+            
+                for(var j =0; j < data[i].artists.length; j++){
+                    console.log(data[i].artists[j].name); 
+                }
+            }
+        }else{
+            spotify.search({ type: 'track', query: "I Want it That Way"}, function(err, data){
+                var data = data.tracks.items;
+                console.log(data[0].name); 
+                console.log(data[0].album.href); 
+                console.log(data[0].album.name); 
+                console.log(data[0].preview_url); 
+                console.log(data[0].artists[0].name); 
+            });
+        }
+    });
+    outputText();
+}
